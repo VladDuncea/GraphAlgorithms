9ac25daa6cda145d9b7fec86e97696edd9161d68
VladDuncea <vlad_duncea_31@yahoo.com>
2019-05-20 00:05:34 +0300

    Havel-Hakimi

--- MinimumSpanningTree/havel_hakimi.cpp
+++ MinimumSpanningTree/havel_hakimi.cpp
@@ -1,25 +1,12 @@
-#if 0
 #include <iostream>
-#include <fstream>
 #include <vector>
 #include <queue>
-#include "DisjointSet.h"
 using namespace std;
-
-struct edge_weight
-{
-	int x, y, c;
-};
 
 struct edge
 {
 	int x, y;
 };
-
-bool cmp(edge_weight a, edge_weight b)
-{
-	return a.c < b.c;
-}
 
 #pragma region Havel-Hakimi
 
@@ -109,81 +96,6 @@ vector<edge> havel_hakimi(vector<int> seq)
 
 #pragma endregion
 
-#pragma region Kruskal
-void kruskal(vector<edge_weight> edges, int n)
-{
-	//Create the disjoint set
-	DisjointSet dj(n);
-	sort(edges.begin(), edges.end(), cmp);
-	vector<edge_weight> answer;
-	for (auto e : edges)
-	{
-		if (dj.getFather(e.x) != dj.getFather(e.y))
-		{
-			dj.link(dj.getFather(e.x), dj.getFather(e.y));
-			answer.push_back(e);
-		}
-	}
-	//print result on screen
-	for (auto e : answer)
-	{
-		cout << (char)(e.x + 'a') << " " << (char)(e.y + 'a') << endl;
-	}
-}
-
-#pragma endregion
-
-#pragma region Prim
-/*
-void prim(vector<edge> edges)
-{
-	vector<bool> vizitat(n + 1, 0);
-	vizitat[1] = 1;
-	vector<edge> answer;
-	edge chosen;
-	priority_queue<edge> Q;
-	for (edge e : edges)
-		if (vizitat[e.x] == 1 || vizitat[e.y] == 1)
-			Q.push(e);
-
-	for (int i = 0; i < n; i++)
-	{
-		int mn = INFINITY;
-		//Optiunea 1
-		for (auto e : edges)
-		{
-			if (vizitat[e.x] + vizitat[e.y] == 1 && e.c<mn)
-			{
-					mn = e.c;
-					chosen = e;
-			}
-		}
-		//Optiunea 2 coada de prioritati
-		while (!Q.empty())
-		{
-			chosen = Q.top();
-			Q.pop();
-			if (vizitat[chosen.x] + vizitat[chosen.y] == 1)
-			{
-				if (vizitat[chosen.x] == 0)
-					swap(chosen.x, chosen.y);
-				for (auto e : adiac[chosen.y])
-					Q.push(e);
-			}
-			vizitat[chosen.x] = vizitat[chosen.y] = 1;
-			answer.push_back(chosen);
-
-		}
-
-		//Final
-		vizitat[chosen.x] = vizitat[chosen.y] = 1;
-		answer.push_back(chosen);
-	}
-
-}
-*/
-#pragma endregion
-
 int main()
 {
 	///Havel Hakimi
@@ -199,21 +111,4 @@ int main()
 			cout << e.x + 1 << " " << e.y + 1 << "\n";
 	}
 
-	ifstream fin;
-	fin.open("date.in");
-	int n, m;
-	fin >> n >> m;
-	vector<edge_weight> edges;
-	for (int i = 0; i < m; i++)
-	{
-		int x, y, c;
-		fin >> x >> y >> c;
-		edges.push_back(edge_weight{ x,y,c });
-	}
-
-	///Kruskal
-	//kruskal(edges,n);
-
 }
-
-#endif // 0
